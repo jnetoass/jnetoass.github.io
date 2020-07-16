f042f2cbd09ea7fc19708e39f008ef64ac1dc1a6
jnetoass <68034131+jnetoass@users.noreply.github.com>
2020-07-15 21:46:04 -0300

    Add files via upload

---
+++ broker.js
@@ -0,0 +1,27 @@
+const mosca = require("mosca");
+const server = new mosca.Server({
+  http: {
+    port:3000,
+    bundle: true,
+    static: './'
+  }
+});
+
+// fired when a new client connects
+server.on('clientConnected', function(client) {
+    console.log('client (clientId:', client.id, ') connected');
+});
+
+// fired when a message is received
+server.on('published', function(packet, client) {
+  	console.log("-------------------------------");
+  	console.log('Published', packet.payload);
+  	console.log("-------------------------------");
+});
+
+server.on('ready', setup);
+
+// fired when the mqtt server is ready
+function setup() {
+  console.log('Mosca server is up and running');
+}
